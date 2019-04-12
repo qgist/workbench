9e9bfceeea437502ca4a973173c6a04030169d76
Sebastian M. Ernst <ernst@pleiszenburg.de>
2019-04-12 19:53:06 +0200

    workbench core dtype

--- qgist/workbench/dtype_uielement.py
+++ qgist/workbench/dtype_uielement.py
@@ -8,17 +8,17 @@ class dtype_uielement_class:
         name_translated = '',
         visibility = True,
         existence = True,
-        handle = None,
+        # handle = None,
         ):
 
         if not isinstance(name_internal, str):
-            TypeError('internal name must be str')
+            raise TypeError('internal name must be str')
         if not isinstance(name_translated, str):
-            TypeError('translated name must be str')
+            raise TypeError('translated name must be str')
         if not isinstance(visibility, bool):
-            TypeError('visibility must be bool')
+            raise TypeError('visibility must be bool')
         if not isinstance(existence, bool):
-            TypeError('existence must be bool')
+            raise TypeError('existence must be bool')
 
         if len(name_internal) == 0:
             QgistUnnamedUiELement('Unnamed UI element: "%s" / "%s"' % (name_internal, name_translated))
@@ -27,7 +27,11 @@ class dtype_uielement_class:
         self._name_translated = name_translated
         self._visibility = visibility
         self._existence = existence
-        self._handle = handle # Handle on actual object
+        # self._handle = handle # Handle on actual object
+
+    def set_state(self, object_handle):
+
+        pass
 
     def asdict(self):
 
--- qgist/workbench/dtype_workbench.py
+++ qgist/workbench/dtype_workbench.py
@@ -1,13 +1,66 @@
 
+import base64
+
+from .dtype_uielement import dtype_uielement_class
 
 class dtype_workbench_class:
 
-    def __init__(self, name):
+    def __init__(self,
+        name = '',
+        state_mainwindow = None,
+        list_toolbars = None,
+        list_dockwidgets = None,
+        main_window = None, # OPTIONAL
+        ):
 
+        if not isinstance(name, str):
+            raise TypeError('name must be str')
+        if len(name) == 0:
+            raise ValueError('name must not be empty')
         self._name = name
+
+        if main_window is not None and any([
+            state_mainwindow is not None,
+            list_toolbars is not None,
+            list_dockwidgets is not None,
+            ]):
+            raise ValueError('provide main_window OR state_mainwindow,list_toolbars,list_dockwidgets')
+
+        if main_window is not None:
+            self._state_mainwindow = bytes(main_window.saveState())
+        elif isinstance(state_mainwindow, str):
+            self._state_mainwindow = base64.decodebytes(state_mainwindow.encode('ASCII'))
+        elif isinstance(state_mainwindow, bytes):
+            self._state_mainwindow = base64.decodebytes(state_mainwindow)
+        else:
+            raise TypeError('state_mainwindow must either be str or bytes')
+
+        if main_window is not None:
+            pass # TODO add defaults
+        elif isinstance(list_toolbars, list) and isinstance(list_dockwidgets, list):
+            if any([not isinstance(item, dict) for item in list_toolbars])
+                raise TypeError('items in list_toolbars must be dicts')
+            if any([not isinstance(item, dict) for item in list_dockwidgets])
+                raise TypeError('items in list_dockwidgets must be dicts')
+            self._list_toolbars = [dtype_uielement_class(**item) for item in list_toolbars]
+            self._list_dockwidgets = [dtype_uielement_class(**item) for item in list_dockwidgets]
+        else:
+            raise TypeError('list_toolbars and list_dockwidgets must be lists')
+
+    def activate(self, main_window):
+
+        tb_list = main_window.findChildren(QToolBar)
+        dw_list = main_window.findChildren(QDockWidget)
+
+        
+
+        main_window.restoreState(self._state_mainwindow)
 
     def asdict(self):
 
         return dict(
             name = self._name,
-            ) # TODO
+            state_mainwindow = base64.encodebytes(self._state_mainwindow).decode('ASCII'),
+            list_toolbars = [item.asdict() for item in self._list_toolbars],
+            list_dockwidgets = [item.asdict() for item in self._list_toolbars],
+            )
