b0860acef53902a2a8c04c8dea823393a887095e
Sebastian M. Ernst <ernst@pleiszenburg.de>
2019-04-14 11:15:25 +0200

    data model updated to use state of mainwindow

--- qgist/workbench/dtype_workbench.py
+++ qgist/workbench/dtype_workbench.py
@@ -5,6 +5,7 @@ from .dtype_uielement import dtype_uielement_class
 
 from PyQt5.QtWidgets import (
     QDockWidget,
+    QMainWindow,
     QToolBar,
     )
 
@@ -15,7 +16,6 @@ class dtype_workbench_class:
         mainwindow_state = None,
         toolbars_list = None,
         dockwidgets_list = None,
-        main_window = None, # OPTIONAL
         ):
 
         if not isinstance(name, str):
@@ -24,61 +24,61 @@ class dtype_workbench_class:
             raise ValueError('name must not be empty')
         self._name = name
 
-        if main_window is not None and any([
-            mainwindow_state is not None,
-            toolbars_list is not None,
-            dockwidgets_list is not None,
-            ]):
-            raise ValueError('provide main_window OR mainwindow_state,toolbars_list,dockwidgets_list')
-
-        if main_window is not None:
-            self._state_mainwindow = bytes(main_window.saveState())
-        elif isinstance(mainwindow_state, str):
-            self._state_mainwindow = base64.decodebytes(mainwindow_state.encode('ASCII'))
+        if isinstance(mainwindow_state, str):
+            self._mainwindow_state = base64.decodebytes(mainwindow_state.encode('ASCII'))
         elif isinstance(mainwindow_state, bytes):
-            self._state_mainwindow = base64.decodebytes(mainwindow_state)
+            self._mainwindow_state = base64.decodebytes(mainwindow_state)
         else:
             raise TypeError('mainwindow_state must either be str or bytes')
 
-        if main_window is not None:
-            pass # TODO add defaults
-        elif isinstance(toolbars_list, list) and isinstance(dockwidgets_list, list):
-            if any([not isinstance(item, dict) for item in toolbars_list]):
-                raise TypeError('items in toolbars_list must be dicts')
-            if any([not isinstance(item, dict) for item in dockwidgets_list]):
-                raise TypeError('items in dockwidgets_list must be dicts')
-            self._toolbars_dict = {
-                b.name_internal: b
-                for b in (dtype_uielement_class(**a) for a in toolbars_list)
-                }
-            self._dockwidgets_dict = {
-                b.name_internal: b
-                for b in (dtype_uielement_class(**a) for a in dockwidgets_list)
-                }
-        else:
-            raise TypeError('toolbars_list and dockwidgets_list must be lists')
+        if not isinstance(toolbars_list, list):
+            raise TypeError('toolbars_list must be a list')
+        if not isinstance(dockwidgets_list, list):
+            raise TypeError('dockwidgets_list must be a list')
+        if any([not isinstance(item, dict) for item in toolbars_list]):
+            raise TypeError('items in toolbars_list must be dicts')
+        if any([not isinstance(item, dict) for item in dockwidgets_list]):
+            raise TypeError('items in dockwidgets_list must be dicts')
+        self._toolbars_dict = {
+            b.name_internal: b
+            for b in (dtype_uielement_class(**a) for a in toolbars_list)
+            }
+        self._dockwidgets_dict = {
+            b.name_internal: b
+            for b in (dtype_uielement_class(**a) for a in dockwidgets_list)
+            }
 
-    def activate(self, main_window):
+    def activate(self, mainwindow):
 
-        qtoolbars_dict = self._get_uielements_from_mainwindow(main_window, QToolBar)
-        qdockwidgets_dict = self._get_uielements_from_mainwindow(main_window, QDockWidget)
+        if not isinstance(mainwindow, QMainWindow):
+            raise TypeError('mainwindow must be a QGis mainwindow')
 
-        self._activate_uielements(qtoolbars_dict, self._toolbars_dict)
-        self._activate_uielements(qdockwidgets_dict, self._dockwidgets_dict)
+        qtoolbars_dict = dtype_workbench_class._get_uielements_from_mainwindow(mainwindow, QToolBar)
+        qdockwidgets_dict = dtype_workbench_class._get_uielements_from_mainwindow(mainwindow, QDockWidget)
 
-        main_window.restoreState(self._state_mainwindow) # TODO ... check!
+        dtype_workbench_class._activate_uielements(qtoolbars_dict, self._toolbars_dict)
+        dtype_workbench_class._activate_uielements(qdockwidgets_dict, self._dockwidgets_dict)
+
+        mainwindow.restoreState(self._mainwindow_state) # TODO ... check!
+
+    def deactivate(self, mainwindow):
+
+        if not isinstance(mainwindow, QMainWindow):
+            raise TypeError('mainwindow must be a QGis mainwindow')
+
+        self._mainwindow_state = mainwindow.saveState() # TODO ... check!
 
     def as_dict(self):
 
         return dict(
             name = self._name,
-            mainwindow_state = base64.encodebytes(self._state_mainwindow).decode('ASCII'),
+            mainwindow_state = base64.encodebytes(self._mainwindow_state).decode('ASCII'),
             toolbars_list = [item.as_dict() for item in self._toolbars_dict.values()],
             dockwidgets_list = [item.as_dict() for item in self._dockwidgets_dict.values()],
             )
 
     @staticmethod
-    def _activate_uielements(uiobjects_dict, uielements_dict)
+    def _activate_uielements(uiobjects_dict, uielements_dict):
 
         for name_internal, uiobject in uiobjects_dict:
             try:
@@ -90,28 +90,38 @@ class dtype_workbench_class:
             uielements_dict[name_internal].existence = False
 
     @staticmethod
-    def _get_uielements_from_mainwindow(main_window, uielement_type):
+    def _get_uielements_from_mainwindow(mainwindow, uielement_type):
 
         return {
             toolbar.objectName(): toolbar
-            for toolbar in main_window.findChildren(uielement_type)
+            for toolbar in mainwindow.findChildren(uielement_type)
             if toolbar.parent().objectName() == 'QgisApp'
             }
 
     @staticmethod
-    def from_data(
-        name = '',
-        mainwindow_state = None,
-        toolbars_list = None,
-        dockwidgets_list = None,
-        ):
-
-        return dtype_workbench_class(name, mainwindow_state, toolbars_list, dockwidgets_list)
-
-    @staticmethod
     def from_mainwindow(
         name = '',
-        main_window = None,
+        mainwindow = None,
         ):
 
-        return dtype_workbench_class(name, main_window = main_window)
+        if not isinstance(mainwindow, QMainWindow):
+            raise TypeError('mainwindow must be a QGis mainwindow')
+
+        toolbars_list = [
+            dtype_uielement_class.from_uiobject(uiobject)
+            for _, uiobject in dtype_workbench_class._get_uielements_from_mainwindow(mainwindow, QToolBar)
+            ]
+
+        dockwidgets_list = [
+            dtype_uielement_class.from_uiobject(uiobject)
+            for _, uiobject in dtype_workbench_class._get_uielements_from_mainwindow(mainwindow, QDockWidget)
+            ]
+
+        mainwindow_state = mainwindow.saveState()
+
+        return dtype_workbench_class(
+            name = name,
+            mainwindow_state = mainwindow_state,
+            toolbars_list = toolbars_list,
+            dockwidgets_list = dockwidgets_list,
+            )
