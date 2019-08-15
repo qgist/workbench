0afbfd2d549c6042fb73c844ee9a510e37e0c928
Sebastian M. Ernst <ernst@pleiszenburg.de>
2019-08-15 10:52:54 +0200

    rm notes

--- qgist/workbench/__init__.py
+++ qgist/workbench/__init__.py
@@ -1,30 +1,4 @@
 
-"""
-
-# Only those are enable which are having "QGistApp" as parent
-# i.e. only toolbars from QGIS main window and not from sub widgets.
-currentToolBarWidget = [
-    toolbar for toolbar in self.mMainWindow.findChildren(
-        QToolBar, toolBarName
-    ) if toolbar.parent().objectName() == 'QgisApp'
-]
-
-mw = iface.mainWindow()
-tb_list = mw.findChildren(QToolBar)
-dw_list = mw.findChildren(QDockWidget)
-
-tb_list[0].windowTitle() # übersetzt
-tb_list[0].objectName() # intern
-dw_list[0].windowTitle()
-dw_list[0].objectName()
-
-sm = [i for i in mw.findChildren(QMenu) if i.objectName() == 'mSettingsMenu'][0]
-sm_actions = [i for i in sm.actions()]
-
-h[1].text() # dockwidgets
-h[2].text() # toolbars
-
-"""
 
 class workbench:
 
