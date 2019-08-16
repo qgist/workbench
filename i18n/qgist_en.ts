794fb2176879ff5be30155fc6eb2aae1775abd31
Sebastian M. Ernst <ernst@pleiszenburg.de>
2019-08-16 13:20:02 +0200

    added translations

---
+++ i18n/qgist_en.ts
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0" language="en" sourcelanguage="en">
+<context>
+    <name>global</name>
+    <message>
+        <location filename="../qgist/workbench/core.py" line="112"/>
+        <source>Qgist Work&amp;Bench</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/manager_ui_base.py" line="104"/>
+        <source>New work bench</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/manager_ui_base.py" line="105"/>
+        <source>Delete work bench</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/manager_ui_base.py" line="106"/>
+        <source>Save work benches</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/manager_ui_base.py" line="73"/>
+        <source>Workbench Configuration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_fsm.py" line="79"/>
+        <source>user default</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/core.py" line="109"/>
+        <source>&amp;Workbench Management</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/core.py" line="122"/>
+        <source>Manage workbenches</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
