451eb71afda97ea30e54ffca003c78c2363c67d0
Sebastian M. Ernst <ernst@pleiszenburg.de>
2019-08-16 10:57:23 +0200

    state machine, first draft

--- qgist/workbench/fsm.py
+++ qgist/workbench/fsm.py
@@ -33,11 +33,20 @@ import os
 
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# IMPORT (External Dependencies)
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+from PyQt5.QtWidgets import (
+    QMainWindow,
+    )
+
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # IMPORT (Internal)
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-from .dtype_uielement import dtype_uielement_class
 from .dtype_workbench import dtype_workbench_class
+from ..util import translate
 
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -46,46 +55,110 @@ from .dtype_workbench import dtype_workbench_class
 
 class fsm_class:
 
-    def __init__(self, workbench_list):
+    def __init__(self, workbench_list, mainwindow, last_workbench = None):
 
         if not isinstance(workbench_list, list):
             raise TypeError('workbench_list must be a list')
         if any([not isinstance(item, dict) for item in workbench_list]):
             raise TypeError('items in workbench_list must be dicts')
+        if not isinstance(mainwindow, QMainWindow):
+            raise TypeError('mainwindow must be a QGis mainwindow')
+        if not isinstance(last_workbench, str) and last_workbench is not None:
+            raise TypeError('last_workbench must be a str or None')
+        if last_workbench is not None and len(workbench_list) == 0:
+            raise ValueError('last_workbench is not None while workbench_list is empty')
+        if last_workbench is not None and last_workbench not in self._workbench_dict.keys():
+            raise ValueError('last_workbench does not exist')
 
         self._workbench_dict = {item['name']: dtype_workbench_class(**item) for item in workbench_list}
+        self._active_workbench = None
+
+        self.keys = self._workbench_dict.keys
+
+        if len(self) == 0:
+            self.new_workbench(translate('global', 'user default'), mainwindow)
+        elif last_workbench is not None:
+            self.activate_workbench(last_workbench, mainwindow)
+        else:
+            self.activate_workbench(tuple(self._workbench_dict.keys())[0], mainwindow)
+
+    def __len__(self):
+
+        return len(self._workbench_dict)
 
     def as_list(self):
 
         return [item.as_dict() for item in self._workbench_dict.values()]
 
-    def activate_workbench(self, name):
-        pass
+    def activate_workbench(self, name, mainwindow, force = False):
 
-    def new_workbench(self, name):
-        pass
+        if not isinstance(name, str):
+            raise TypeError('name must be str')
+        if not isinstance(mainwindow, QMainWindow):
+            raise TypeError('mainwindow must be a QGis mainwindow')
 
-    def delete_workbench(self, name):
-        pass
+        if name not in self._workbench_dict.keys():
+            raise ValueError('name is not a known workbench')
+        if self._active_workbench == name and not force:
+            return
 
-    def save_workbench(self, name):
-        pass
+        self._workbench_dict[name].activate(mainwindow)
+        self._active_workbench = name
 
-    @staticmethod
-    def from_config_file(fn):
+    def new_workbench(self, name, mainwindow):
 
-        if not isinstance(fn, str):
-            raise TypeError('fn must be str')
-        if not os.path.exists(fn):
-            raise ValueError('fn must exists')
-        if not os.path.isfile(fn):
-            raise ValueError('fn must be a file')
+        if not isinstance(name, str):
+            raise TypeError('name must be str')
+        if not isinstance(mainwindow, QMainWindow):
+            raise TypeError('mainwindow must be a QGis mainwindow')
 
-        with open(fn, 'r', encoding = 'utf-8') as f:
-            cfg_data = json.loads(f.read())
+        if name in self._workbench_dict.keys():
+            raise ValueError('name is a known workbench, i.e. already exists')
+        if len(name) == 0:
+            raise ValueError('name is empty')
 
-        if not isinstance(cfg_data, dict):
-            raise TypeError('configuration must be a dict')
-        workbench_list = cfg_data.get('workbenchs', list())
+        self._workbench_dict[name] = dtype_workbench_class.from_mainwindow(name, mainwindow)
+        self._active_workbench = name
 
-        return fsm_class(workbench_list)
+    def delete_workbench(self, name, mainwindow):
+
+        if not isinstance(name, str):
+            raise TypeError('name must be str')
+        if not isinstance(mainwindow, QMainWindow):
+            raise TypeError('mainwindow must be a QGis mainwindow')
+
+        if name not in self._workbench_dict.keys():
+            raise ValueError('name is not a known workbench')
+        if len(self) <= 1:
+            raise ValueError('only one workbench left, can not be deleted')
+        if name == self._active_workbench:
+            other_name = tuple(self._workbench_dict.keys() - set((name,)))[0]
+            self.activate_workbench(other_name, mainwindow)
+
+        self._workbench_dict.pop(name)
+
+    def rename_workbench(self, old_name, new_name, mainwindow):
+
+        pass # TODO
+
+    def save_workbench(self, name, mainwindow):
+
+        if not isinstance(name, str):
+            raise TypeError('name must be str')
+        if not isinstance(mainwindow, QMainWindow):
+            raise TypeError('mainwindow must be a QGis mainwindow')
+
+        if self._active_workbench != name:
+            raise ValueError('workbench must be active for being saved')
+
+        self._workbench_dict[name].save(mainwindow)
+
+    @property
+    def active_workbench(self):
+
+        return self._active_workbench
+
+    @active_workbench.setter
+    def active_workbench(self, value):
+
+        raise AttributeError('active_workbench must not be changed')
