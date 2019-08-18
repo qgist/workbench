f78e312c78dd6f89a4ed2e5abe65a1f3adf5fd47
Sebastian M. Ernst <ernst@pleiszenburg.de>
2019-08-18 13:42:43 +0200

    updated translation

--- i18n/qgist_en.ts
+++ i18n/qgist_en.ts
@@ -3,11 +3,6 @@
 <context>
     <name>global</name>
     <message>
-        <location filename="../qgist/workbench/ui_manager.py" line="134"/>
-        <source>New work bench</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <location filename="../qgist/workbench/ui_manager_base.py" line="74"/>
         <source>Workbench Configuration</source>
         <translation type="unfinished"></translation>
@@ -70,11 +65,6 @@
     <message>
         <location filename="../qgist/msg.py" line="53"/>
         <source>Warning</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qgist/msg.py" line="63"/>
-        <source>Internal error. No description can be provided. Please file a bug.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -250,6 +240,281 @@
     <message>
         <location filename="../qgist/workbench/core.py" line="121"/>
         <source>Qgist Work&amp;bench</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/msg.py" line="58"/>
+        <source>&quot;exception&quot; must be of type Exception. (msg)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/msg.py" line="60"/>
+        <source>&quot;widget&quot; must be of type QWidget or None. (msg)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/msg.py" line="63"/>
+        <source>Internal error. No description can be provided. Please file a bug. (msg)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/config.py" line="67"/>
+        <source>QGIS settings path does not point to a directory. (config path)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/config.py" line="69"/>
+        <source>QGIS settings path does not exist. (config path)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/config.py" line="73"/>
+        <source>QGIS plugin configuration path exists but is not a directory. (config path)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/config.py" line="79"/>
+        <source>QGIST configuration path exists but is not a directory. (config path)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/config.py" line="95"/>
+        <source>&quot;fn&quot; must be str. (config)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/config.py" line="101"/>
+        <source>Parent of &quot;fn&quot; must exists. (config)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/config.py" line="103"/>
+        <source>Parent of &quot;fn&quot; must be a directory. (config)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/config.py" line="108"/>
+        <source>&quot;fn&quot; must be a file. (config)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/config.py" line="112"/>
+        <source>Configuration data must be a dict. (config)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/config.py" line="117"/>
+        <source>&quot;name&quot; must be str. (config getitem)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/config.py" line="119"/>
+        <source>Unknown configuration field &quot;name&quot;. (config getitem)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/config.py" line="126"/>
+        <source>&quot;name&quot; must be str. (config setitem)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/config.py" line="128"/>
+        <source>&quot;value&quot; contains not allowed types. (config setitem)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/config.py" line="174"/>
+        <source>Could not backup old configuration before saving new - too many old backups. (config save)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_uielement.py" line="63"/>
+        <source>&quot;internal_name&quot; must be str. (dtype_uielement)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_uielement.py" line="65"/>
+        <source>Unnamed UI element. (dtype_uielement)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_uielement.py" line="69"/>
+        <source>&quot;translated_name&quot; must be str. (dtype_uielement)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_uielement.py" line="73"/>
+        <source>&quot;visibility&quot; must be bool. (dtype_uielement)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_uielement.py" line="78"/>
+        <source>&quot;uiobject&quot; must be either QToolBar or QDockWidget. (dtype_uielement)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_uielement.py" line="115"/>
+        <source>&quot;value&quot; must be bool. (dtype_uielement setVisible)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_uielement.py" line="131"/>
+        <source>&quot;value&quot; must be bool. (dtype_uielement existence)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_uielement.py" line="133"/>
+        <source>&quot;value&quot; can only be set to false. (dtype_uielement existence)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_uielement.py" line="146"/>
+        <source>&quot;name_internal&quot; must not be changed. (dtype_uielement name_internal)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_uielement.py" line="156"/>
+        <source>&quot;name_translated&quot; must not be changed. (dtype_uielement name_internal)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_uielement.py" line="167"/>
+        <source>&quot;visibility&quot; must be bool. (dtype_uielement visibility)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_uielement.py" line="175"/>
+        <source>&quot;uiobject&quot; must be either QToolBar or QDockWidget. (dtype_uielement from_uiobject)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/ui_manager.py" line="77"/>
+        <source>&quot;plugin_root_fld&quot; must be str. (ui_manager)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/ui_manager.py" line="79"/>
+        <source>&quot;plugin_root_fld&quot; must exists. (ui_manager)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/ui_manager.py" line="81"/>
+        <source>&quot;plugin_root_fld&quot; must be a directory. (ui_manager)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/ui_manager.py" line="83"/>
+        <source>&quot;mainwindow&quot; must be a QGIS mainwindow. (ui_manager)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/ui_manager.py" line="85"/>
+        <source>&quot;combobox_workbench&quot; must be a QGis mainwindow. (ui_manager)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/ui_manager.py" line="87"/>
+        <source>&quot;combobox_workbench_update&quot; must be callable. (ui_manager)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/ui_manager.py" line="89"/>
+        <source>&quot;fsm&quot; must be a workbench finite state machine. (ui_manager)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_workbench.py" line="72"/>
+        <source>&quot;name&quot; must be str. (dtype_workbench)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_workbench.py" line="74"/>
+        <source>&quot;name&quot; must not be empty. (dtype_workbench)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_workbench.py" line="82"/>
+        <source>&quot;mainwindow_state&quot; must either be str or bytes. (dtype_workbench)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_workbench.py" line="85"/>
+        <source>&quot;mainwindow&quot; must be a QGIS mainwindow. (dtype_workbench)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_workbench.py" line="88"/>
+        <source>&quot;toolbars_list&quot; must be a list. (dtype_workbench)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_workbench.py" line="90"/>
+        <source>&quot;dockwidgets_list&quot; must be a list. (dtype_workbench)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_workbench.py" line="92"/>
+        <source>Items in &quot;toolbars_list&quot; must be dicts. (dtype_workbench)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_workbench.py" line="94"/>
+        <source>Items in &quot;dockwidgets_list&quot; must be dicts. (dtype_workbench)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_workbench.py" line="117"/>
+        <source>Not enough parameters, two expected. (dtype_workbench item)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_workbench.py" line="119"/>
+        <source>Wrong number of parameters, two expected. (dtype_workbench item)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_workbench.py" line="124"/>
+        <source>&quot;item_type&quot; must be str. (dtype_workbench item)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_workbench.py" line="126"/>
+        <source>Unknown &quot;item_type&quot;. (dtype_workbench item)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_workbench.py" line="131"/>
+        <source>&quot;item_name&quot; must be str. (dtype_workbench item)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_workbench.py" line="133"/>
+        <source>&quot;item_name&quot; is not a known item. (dtype_workbench item)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_workbench.py" line="140"/>
+        <source>&quot;mainwindow&quot; must be a QGIS mainwindow. (dtype_workbench activate)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_workbench.py" line="153"/>
+        <source>&quot;mainwindow&quot; must be a QGIS mainwindow. (dtype_workbench save)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_workbench.py" line="222"/>
+        <source>New value of &quot;name&quot; must be a str. (dtype_workbench name)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_workbench.py" line="224"/>
+        <source>New value of &quot;name&quot; must not be empty. (dtype_workbench name)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_workbench.py" line="235"/>
+        <source>&quot;mainwindow&quot; must be a QGIS mainwindow. (dtype_workbench from_mainwindow)</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
