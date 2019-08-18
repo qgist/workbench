f812dc00787b15460e47de0364b25b2a7bdd080e
Sebastian M. Ernst <ernst@pleiszenburg.de>
2019-08-18 13:17:25 +0200

    update translations

--- i18n/qgist_en.ts
+++ i18n/qgist_en.ts
@@ -3,11 +3,6 @@
 <context>
     <name>global</name>
     <message>
-        <location filename="../qgist/workbench/core.py" line="121"/>
-        <source>Qgist Work&amp;Bench</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <location filename="../qgist/workbench/ui_manager.py" line="134"/>
         <source>New work bench</source>
         <translation type="unfinished"></translation>
@@ -80,6 +75,181 @@
     <message>
         <location filename="../qgist/msg.py" line="63"/>
         <source>Internal error. No description can be provided. Please file a bug.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_fsm.py" line="60"/>
+        <source>&quot;workbench_list&quot; must be a list. (dtype_fsm)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_fsm.py" line="62"/>
+        <source>Items in workbench_list must be dicts. (dtype_fsm)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_fsm.py" line="64"/>
+        <source>&quot;mainwindow&quot; must be a QGis mainwindow. (dtype_fsm)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_fsm.py" line="66"/>
+        <source>&quot;active_workbench&quot; must be a str or None. (dtype_fsm)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_fsm.py" line="68"/>
+        <source>&quot;active_workbench&quot; is not None while &quot;workbench_list&quot; is empty. (dtype_fsm)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_fsm.py" line="70"/>
+        <source>&quot;config&quot; must be a &quot;config_class&quot; object or None. (dtype_fsm)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_fsm.py" line="79"/>
+        <source>&quot;active_workbench&quot; does not exist. (dtype_fsm)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_fsm.py" line="95"/>
+        <source>&quot;name&quot; must be str. (dtype_fsm item)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_fsm.py" line="97"/>
+        <source>&quot;name&quot; is not a known workbench. (dtype_fsm item)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_fsm.py" line="112"/>
+        <source>&quot;name&quot; must be str. (dtype_fsm activate)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_fsm.py" line="114"/>
+        <source>&quot;mainwindow&quot; must be a QGis mainwindow. (dtype_fsm activate)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_fsm.py" line="117"/>
+        <source>&quot;name&quot; is not a known workbench. (dtype_fsm activate)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_fsm.py" line="129"/>
+        <source>&quot;name&quot; must be str. (dtype_fsm new)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_fsm.py" line="131"/>
+        <source>&quot;mainwindow&quot; must be a QGis mainwindow. (dtype_fsm new)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_fsm.py" line="134"/>
+        <source>&quot;name&quot; is a known workbench, i.e. already exists. (dtype_fsm new)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_fsm.py" line="136"/>
+        <source>&quot;name&quot; is empty. (dtype_fsm new)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_fsm.py" line="146"/>
+        <source>&quot;name&quot; must be str. (dtype_fsm delete)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_fsm.py" line="148"/>
+        <source>&quot;mainwindow&quot; must be a QGIS mainwindow. (dtype_fsm delete)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_fsm.py" line="151"/>
+        <source>&quot;name&quot; is not a known workbench. (dtype_fsm delete)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_fsm.py" line="153"/>
+        <source>only one workbench left, can not be deleted. (dtype_fsm delete)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_fsm.py" line="165"/>
+        <source>&quot;old_name&quot; must be str. (dtype_fsm rename)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_fsm.py" line="167"/>
+        <source>&quot;new_name&quot; must be str. (dtype_fsm rename)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_fsm.py" line="169"/>
+        <source>&quot;mainwindow&quot; must be a QGis mainwindow. (dtype_fsm rename)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_fsm.py" line="171"/>
+        <source>&quot;old_name&quot; is not a known workbench. (dtype_fsm rename)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_fsm.py" line="173"/>
+        <source>&quot;new_name&quot; is a known workbench, i.e. already exists. (dtype_fsm rename)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_fsm.py" line="175"/>
+        <source>&quot;new_name&quot; is empty. (dtype_fsm rename)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_fsm.py" line="190"/>
+        <source>&quot;name&quot; must be str. (dtype_fsm save)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_fsm.py" line="192"/>
+        <source>&quot;mainwindow&quot; must be a QGis mainwindow. (dtype_fsm save)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_fsm.py" line="195"/>
+        <source>&quot;workbench&quot; must be active for being saved. (dtype_fsm save)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/dtype_fsm.py" line="217"/>
+        <source>&quot;active_workbench&quot; must not be changed. (dtype_fsm active)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/core.py" line="92"/>
+        <source>&quot;iface&quot; must be a QGIS iface object. (workbench)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/core.py" line="94"/>
+        <source>&quot;plugin_root_fld&quot; must be str. (workbench)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/core.py" line="96"/>
+        <source>&quot;plugin_root_fld&quot; must exists. (workbench)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/core.py" line="98"/>
+        <source>&quot;plugin_root_fld&quot; must be a directory. (workbench)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qgist/workbench/core.py" line="121"/>
+        <source>Qgist Work&amp;bench</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
